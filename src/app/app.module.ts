2a9997c176fcebafdf579459859035c92e55c87d
Bezael <bezael_e@hotmail.com>
2019-10-16 22:56:29 +0200

    Just one commit sorry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { PaymentsComponent } from './components/payments/payments.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxStripeModule } from 'ngx-stripe';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, PaymentsComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    NgxStripeModule.forRoot('pk_test_52DU0HNaT3yQjlK5Mg5H6SER00DJsQHl0U')
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
